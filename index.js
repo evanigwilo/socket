b9b59b23a649492c535324ce06005719976f7253
Evan Igwilo <uvclassic@gmail.com>
2022-11-11 18:52:36 +0000

    ONLY COMMIT 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const express = require('express');
 const { Server } = require("socket.io");
+var path = require('path');
 
 const http = require('http');
 const app = express();
@@ -15,14 +16,20 @@ const io = new Server(server, {
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile((__dirname + '/client/dist/index.html').replace("server/", ""));
+// });
+// console.log(path.join(__dirname, '../client/dist/index.html'));
+//app.use(express.static(__dirname)); // Current directory is root
+
+//app.use(express.static(path.join(__dirname, '../client/dist'))); //  "public" off of current is root
+
+app.use(express.static(path.join(__dirname, '/dist')));
 
 io.on("connection", (socket) => {
     socket.emit("setID", socket.id);
 
-    console.log(socket.id)
+    console.log(socket.id);
 
     // socket.on("disconnect", () => {
     //     socket.broadcast.emit("callEnded")
@@ -34,7 +41,7 @@ io.on("connection", (socket) => {
     });
 
     socket.on("answerCall", ({ calling, signalData }) => {
-        io.to(calling).emit("answerCall", { signalData })
+        io.to(calling).emit("answerCall", { signalData });
     });
 
     socket.on("sentOffer", ({ calling, from, signalData }) => {
